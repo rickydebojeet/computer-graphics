5a29c599e14c79f106a97d32159ba99abcfc01b3
Ricky <debojeetofficial@gmail.com>
2021-08-02 13:37:51 +0530

    :memo: NEW: final bresenham

--- src/line/bresenham.c
+++ src/line/bresenham.c
@@ -3,6 +3,11 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
 
 // Function Defination
 void myInit();
@@ -122,6 +127,7 @@ void bresenham()
             // The points are specified
             glEnd();
             glFlush();
+            usleep(1000);
         }
     }
     else{
@@ -142,6 +148,7 @@ void bresenham()
             // The points are specified
             glEnd();
             glFlush();
+            usleep(1000);
         }
     }
 }
